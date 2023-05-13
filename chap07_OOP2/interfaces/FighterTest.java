569a74a17cacc0e7e61cc97f249748ebd09289d9
zeze <thwjd2717@gmail.com>
2023-05-14 00:04:08 +0900

    7장 인터페이스 설명(주석) 추가

--- chap07_OOP2/interfaces/FighterTest.java
+++ chap07_OOP2/interfaces/FighterTest.java
@@ -24,7 +24,7 @@ class FighterTest {
 
 class Fighter extends Unit implements Fightable {
     public void move(int x, int y) { /* 내용 생략 */ }      // 상속받은 인터페이스를 구현(implements)하는 메소드
-    public void attack(Unit u) { /* 내용 생략 */ }
+    public void attack(Unit u) { /* 내용 생략 */ }          // 조상 인터페이스(Attackable)의 메소드를 오버라이딩하여 조상 메소드보다 접근제어자의 범위가 더 넓어야함(public)
 }
 
 class Unit {
@@ -36,4 +36,4 @@ class Unit {
 interface Fightable extends Movable, Attackable { }     // 인터페이스 Fightable 자체는 정의된 멤버가 하나도 없지만,
                                                         // 조상 인터페이스(Movable, Attackable)에 정의된 멤버를 모두 상속받음
 interface Movable {	void move(int x, int y); }
-interface Attackable { void attack(Unit u); }
+interface Attackable { void attack(Unit u); }           // 인터페이스의 기본 메소드 접근제어자는 public abstract로 생략 가능
