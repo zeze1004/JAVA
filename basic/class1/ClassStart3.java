e0fa133c840d2b83a39e615e12b03e313bb85d07
zeze <thwjd2717@gmail.com>
2023-11-30 17:27:11 +0900

    refactor: 배열에 인스턴스 주소값을 참조시켜 iter로 출력하기

--- basic/class1/ClassStart3.java
+++ basic/class1/ClassStart3.java
@@ -13,12 +13,17 @@ public class ClassStart3 {
         student2.age = 15;
         student2.grade = 90;
 
-        Student[] students = new Student[2]; // students = 메모리 주소값, 해당 메모리에 두 개의 배열 공간 존재[null, null]
-                                             // 자바에서 대입은 항상 변수에 들어 있는 값을 복사
-        students[0] = student1;
-        students[1] = student2;
+        // Student[] students = new Student[2]; // students = 메모리 주소값, 해당 메모리에 두 개의 배열 공간 존재[null, null]
+        //                                      // 자바에서 대입은 항상 변수에 들어 있는 값을 복사
+        // students[0] = student1;
+        // students[1] = student2;
 
-        System.out.println("이름:" + students[0].name + " 나이:" + students[0].age + " 성적:" + students[0].grade);
-        System.out.println("이름:" + students[1].name + " 나이:" + students[1].age + " 성적:" + students[1].grade);
+        // 생성과 선언을 동시에 하는 경우
+        Student[] students = { student1, student2 };
+
+        // Student s를 선언하여 배열의 참조값을 전달
+        for (Student s : students) { // Student s = students[i];
+            System.out.println("이름: " + s.name + " 나이: " + s.age + " 성적: " + s.grade);
+        }
     }
 }
