7a9db6ebec6d000659437e2a96e6f38c54d5d450
zeze <thwjd2717@gmail.com>
2023-12-01 23:57:04 +0900

    feat: 접근제어자 예제 스피커 샘플 코드 작성

---
+++ basic/access/Speaker.java
@@ -0,0 +1,27 @@
+package basic.access;
+
+public class Speaker {
+    int volume;
+
+    public Speaker(int volume) {
+        this.volume = volume;
+    }
+
+    void volumeUp() {
+        if (volume >= 100) {
+            System.out.println("음량을 증가할 수 없음. 최대 용량임");
+        } else {
+            volume += 10;
+            System.out.println("음량을 10 증가");
+        }
+    }
+
+    void volumeDown() {
+        volume -= 10;
+        System.out.println("volumedown 호출");
+    }
+
+    void showVolume() {
+        System.out.println("현재 음량: " + volume);
+    }
+}
