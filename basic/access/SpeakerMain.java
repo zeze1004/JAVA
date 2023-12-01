a7c843853f50eb53d463943e5c8fafa9e3e1f048
zeze <thwjd2717@gmail.com>
2023-12-02 00:03:04 +0900

    feat: [접근제어자] 스피커 메인 코드

---
+++ basic/access/SpeakerMain.java
@@ -0,0 +1,14 @@
+package basic.access;
+
+public class SpeakerMain {
+    public static void main(String[] args) {
+        Speaker speaker = new Speaker(90);
+        speaker.showVolume();
+
+        speaker.volumeUp();
+        speaker.showVolume();
+
+        speaker.volumeUp();
+        speaker.showVolume();
+    }
+}
